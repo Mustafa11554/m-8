370057ce7a73c8c66e03a22ffae2a8977ae8b257
Mustafa <mustafa01999@icloud.com>
2023-08-23 22:06:47 +0300

    fwferw

---
+++ main.js
@@ -0,0 +1,24 @@
+const a = []
+debugger
+for(let i = 0; i <= 1000; i++){
+     2 === 0;
+     {
+        a.push(i)
+    }
+}
+const nav = [];
+for(let i = 0; i < a.length; i++){
+    if(i % 3 === 0){;
+        nav.push(i)
+    };
+}
+
+
+const stepen = nav.map(function(value, index){
+    return value ** 2;
+
+
+});
+
+
+console.log(nav)
